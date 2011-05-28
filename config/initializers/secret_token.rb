a9ceb7df94aaa51ba4b5002d7e766a4a9ddf0666
Amit Aharoni <AmitAharoni@Amit-Aharonis-MacBook-Air.local>
2011-05-28 15:14:52 -0700

    Bare bone Rails app. MVC framework. jQuery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jstennis::Application.config.secret_token = '084ef04c3feee5f571de349b7c3c46fe2b6e3eda1adcb4e7b2b259267e6e8ddbf15cc6be7534ce88858488279118e9b2ac323bd752a33427524a9404796cc198'
